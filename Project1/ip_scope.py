15c836482a27ed2b8af058158aaa727c529cf4fc
Dev <Dev@Michaels-MBP-2020.local>
2021-12-15 14:01:13 -0800

    validate_ip_address function implemented to validate input for single or range of IP's

--- Project1/ip_scope.py
+++ Project1/ip_scope.py
@@ -27,9 +27,38 @@ def main(ip):
     geo_data = get_ip_info(ip)
     print_results(geo_data)
 
+def validate_ip_address(address):
+    """Takes in a value and checks for valid ipaddress type
 
-def check_ip_for_public(ip):
-    return "Private" if (ipaddress.ip_address(ip).is_private) else "Public"
+    Parameters
+    ----------
+    address : str or int
+        accepts a single ip address as a base 2 int or a str in IPv4/6, binary (packet), and IPv4/6 CIDR format
+        
+    Returns
+    -------
+    IP object
+       Will return an object of type ipaddress.IPv(4/6)Address or ipaddress.IPv(4/6)Network or ValueError
+    """
+    try:
+        ip = ipaddress.ip_address(address)
+        print(f"IP address {address} is a valid {type(ip)} address")
+        return ip 
+    except:
+        print(f"IP address {address} is not a valid IPv4 or IPv6 address.")
+    
+    try:
+        ip = ipaddress.ip_network(address)
+        print(f"IP address {address} is a valid {type(ip)} address")
+        return ip
+    except:
+        print(f"IP address {address} is not a valid IPv4Network or IPv6Network CIDR address.")
+
+    # If address is neither type function will return ValueError but not raise the exception so that the program can output invalid address
+    return ValueError(f"ValueError '{address}' does not appear to be an IPv4 or IPv6 address or network")
+
+# def check_ip_for_public(ip):
+#     return "Private" if (ipaddress.ip_address(ip).is_private) else "Public"
    
 def get_ip_info(ipv4):
     """Gets ownership and geolocation data from ip_address
@@ -66,7 +95,8 @@ def get_ip_info(ipv4):
         else:
             print(f"Request failed with {ipv4}")
             print(f"Message: {data['message']}")
-            sys.exit()
+
+            # sys.exit()
 
     except requests.exceptions.HTTPError as e:
         print("An HTTP Error occured: ", e)
@@ -82,7 +112,6 @@ def get_ip_info(ipv4):
         #For any other unexpected error
         str = traceback.format_exc()
         print(str)
-
 
 def print_results(geo_dict):
     """Prints ownership and geolocation data requested
